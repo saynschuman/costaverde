98d1f6b62b6b72e7a816420df624d3cd0ef25b65
saynschuman <saynschuman@gmail.com>
2017-07-07 18:10:49 +0300

    carousel fixed

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,30 @@
 $(function() {
 	
-	$('#fullpage').fullpage({
-		anchors:['index', 'ourFood', 'about', 'catering', 'contact']
+	$('#fullpage').fullpage({   
+		anchors:['index', 'food', 'about', 'catering', 'contact'], 
+	    afterLoad: function (anchorLink, index) {
+	            $('.linck_check').removeClass('active');
+	            $('a[href="#'+anchorLink+'"]').addClass('active');
+	            
+	    },
 	});
+
+
+	$('.slides').slick({
+	  	cssEase: 'linear',
+	  	autoplay: true,
+	  	dots: false,
+	  	autoplaySpeed: 3000,
+	  	prevArrow: '<div class="prev"></div>',
+		nextArrow: '<div class="next"></div>'
+	})
+
+	$('#left_arrow').click(function(e){
+		e.preventDefault();
+	})
+	$('#right_arrow').click(function(e){
+		e.preventDefault();
+	})
 });
 
 
